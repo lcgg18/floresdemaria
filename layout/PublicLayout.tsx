95d38d2719a7a60ad5ca2ee267c67825c0d93569
luis carlos guerra <lcgg18@gmail.com>
2023-10-11 10:46:17 -0500

    ONLINE 5

--- layout/PublicLayout.tsx
+++ layout/PublicLayout.tsx
@@ -42,7 +42,7 @@ const PublicLayout:FC<Props> = ({children, name, content, title='Bienvenido', de
       </main>
       <footer className={styles.footer}>
         <span className={styles.subtitle}>
-        Conjunto Cerrado Flores de Maria - GYG Asesorias y Servicios 2023
+        GYG ASESORIAS Y SERVICIOS - COPYRIGHT ® 2023
         </span>
       </footer>
     </div>
